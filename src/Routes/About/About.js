e3c289e37656ceedfe3a0374ae3fcb113fdd31fe
Divyanshisahu01 <divyanshi.sahu@wehealapp.in>
2023-08-16 12:28:00 +0530

    dyanamic hero component created

--- src/Routes/About/About.js
+++ src/Routes/About/About.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Navbar } from '../../components/Navbar/Navbar'
+import { Hero } from "../../components/Hero/Hero"
+import AboutPic from "../../assets/night.jpg"
+
 
 export const About = () => {
-  return (
-    <div>this is About</div>
-  )
+    return (
+        <>
+            <Hero
+                cName="hero-mid"
+                heroImg={AboutPic}
+                title="About"
+                btnClass="hide"
+            ></Hero>
+            <Navbar></Navbar>
+        </>
+
+    )
 }
--- src/Routes/Home/Home.js
+++ src/Routes/Home/Home.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { Navbar } from '../../components/Navbar/Navbar'
-import {Hero} from "../../components/Hero"
+import { Hero } from "../../components/Hero/Hero"
+import HomePic from "../../assets/Home.jpg"
+
+
 export const Home = () => {
     return (
-        <>  
-        <Hero></Hero>
-        <Navbar></Navbar>
+        <>
+            <Hero
+                cName="hero"
+                heroImg={HomePic}
+                title="Your Journey Your Story"
+                text="Choose your Favourite Destination"
+                buttonText="Travel Plan"
+                url="/"
+                btnClass="show"
+            ></Hero>
+            <Navbar></Navbar>
         </>
 
     )
